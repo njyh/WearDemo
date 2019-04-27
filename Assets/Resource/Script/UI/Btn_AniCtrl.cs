9d48676c094a6f07303eb61ebb7d7763b1b26200
Hamster5295 <hamster5295@163.com>
2019-04-27 14:07:14 +0800

    Improve some UIs

---
+++ Assets/Resource/Script/UI/Btn_AniCtrl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Btn_AniCtrl : MonoBehaviour
+{
+    public Animation ani;
+    public string normal;
+    public string highlight;
+    public string pressed;
+    public string pressEnded;
+
+    public void onNormal()
+    {
+        ani.Play(normal);
+    }
+
+    public void onHighLight()
+    {
+        ani.Play(highlight);
+    }
+
+    public void onPressed()
+    {
+        ani.Play(pressed);
+    }
+
+    public void onPressEnded()
+    {
+        ani.Play(pressEnded);
+    }
+}
---
+++ Assets/Resource/Script/UI/RoleCtrl.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoleCtrl : MonoBehaviour
+{
+    public Image RoleImg;
+    public Text RoleText;
+    public Image ItemImg;
+    public Text ItemText;
+    public Image SkillImg;
+    public Text SkillText;
+
+    private bool isAutoHide = true;
+
+    private void Start()
+    {
+        StartCoroutine(AutoHide());
+    }
+
+    public void setRoleImage(Sprite img)
+    {
+        RoleImg.sprite = img;
+    }
+
+    public void setItemImage(Sprite img)
+    {
+        ItemImg.sprite = img;
+    }
+
+    public void setSkillImage(Sprite img)
+    {
+        SkillImg.sprite = img;
+    }
+
+    public void ActiveAll()
+    {
+        RoleImg.enabled = true;
+        RoleText.enabled = true;
+        ItemImg.enabled = true;
+        ItemText.enabled = true;
+        SkillImg.enabled = true;
+        SkillText.enabled = true;
+    }
+
+    public void setAutoHide(bool a)
+    {
+        isAutoHide = a;
+    }
+
+    IEnumerator AutoHide()
+    {
+        while (isAutoHide)
+        {
+            yield return 0;
+
+            if (RoleImg.sprite != null)
+                RoleText.enabled = false;
+            else
+                RoleText.enabled = true;
+
+            if (ItemImg.sprite != null)
+                ItemText.enabled = false;
+            else
+                ItemText.enabled = true;
+
+            if (SkillImg.sprite != null)
+                SkillText.enabled = false;
+            else
+                SkillText.enabled = true;
+        }
+    }
+}
