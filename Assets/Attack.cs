9836b2c3a1a1f949ae371954c2e5c37f5e171f34
BCYH <3185835784@qq.com>
2019-05-02 17:55:54 +0800

    LineDemo
    1111

---
+++ Assets/Attack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Attack : MonoBehaviour
+{
+	private LineRenderer lineRederer;
+    // Start is called before the first frame update
+    void Start()
+    {
+	lineRederer = GetComponent<LineRenderer>();
+
+	lineRederer.enabled = true;
+
+	lineRederer.useWorldSpace = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRederer.SetPosition(0,transform.position);
+
+            lineRederer.SetPosition(1, Input.mousePosition);
+
+    }
+}
